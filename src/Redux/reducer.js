f53ce0477c9b08feb41dc955121c8ccd8b7d9df3
github username <github email address>
2019-02-26 11:16:53 -0500

    first commit, plowing through redux

---
+++ src/Redux/reducer.js
@@ -0,0 +1,10 @@
+const initialState = { facts: "hotYoga" };
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer;
