40279119bd7eb64812eacf612822b148aa14ea7c
github username <taimurmshah@gmail.com>
2019-07-18 16:50:11 -0400

    got rid of annoying comments in App.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,23 @@
-/*----------------------------I am implementing DFS in my comment-ifying!-----------------------------*/
-
 import React, { Component } from "react";
-/*todo why is this different than index.css?*/
 import "./App.css";
-
-//once a user is logged in.
 import Home from "./Containers/Home";
-
-//container component for list of gameCards.
 import GameListContainer from "./Containers/GameContainers/GameListContainer.js";
-
-//container for the flow of the game play.
 import GamePlaySocketHandler from "./Containers/GameContainers/GamePlaySocketHandler.js";
-
-//navigation among different components; worth reading documentation/practicing more.
 import { Route, Switch, Redirect, withRouter } from "react-router-dom";
-
-/*todo wtf does this component do? is landing where they login/reg?*/
 import Landing from "./Containers/Landing";
 import { connect } from "react-redux";
-
-//to check if the user is already logged in && to check from backend if there are any active & available games.
-/*todo I want to remove from available games the games that are in session/have three users in them.*/
 import { logUserInWithToken, getGames } from "./redux/thunks.js";
 
 class App extends Component {
   componentDidMount() {
     if (localStorage.getItem("token")) {
-      //checking to see if the user is already logged in.
-      /*todo what responsibility do I have to log the user out?*/
       let token = localStorage.getItem("token");
-      /*todo how does this method work?*/
       this.props.logUserInWithToken(token);
     } else {
-      /*todo should I be doing anything else in this code block?*/
       console.log("no token");
     }
   }
 
-  /*todo shouldn't I use this method in the componentDidMount up top? I should test this individually.*/
   isUserLoggedIn = () => {
     return localStorage.getItem("token");
   };
@@ -47,7 +26,6 @@ class App extends Component {
     return (
       <div id="top-div">
         <Switch>
-          {/*general rule, most specific/complex path (word-wise) up top. todo arrow function b/c there's logic in there? I should know why. */}
           <Route
             path="/play-game"
             render={() => {
@@ -55,7 +33,6 @@ class App extends Component {
                 <div>
                   {this.isUserLoggedIn() &&
                   Object.keys(this.props.currentGame).length > 0 ? (
-                    /*todo I want the game to be in play if the user accidentally refreshes. How can I accomplish this? could i potentially implement auth for a specific game? how would I do that?*/
                     <GamePlaySocketHandler />
                   ) : (
                     <Redirect to="/" />
@@ -69,12 +46,10 @@ class App extends Component {
             render={() => {
               return (
                 <div>
-                  {/*just checking if the user is logged in. todo what logic does GameListContainer contain? could I possibly control what games are showed here, thus only showing games that need players?*/
                   this.isUserLoggedIn() ? (
-                    <GameListContainer />
+                  <GameListContainer />
                   ) : (
-                    /*good to keep in mind; this is how Browser-Router redirects users.*/
-                    <Redirect to="/" />
+                  <Redirect to="/" />
                   )}
                 </div>
               );
@@ -89,7 +64,6 @@ class App extends Component {
                   {this.isUserLoggedIn() ? (
                     <Redirect to="/home" />
                   ) : (
-                    /*landing must be the login/reg page.*/
                     <Landing />
                   )}
                 </div>
@@ -97,7 +71,6 @@ class App extends Component {
             }}
           />
         </Switch>
-        {/* todo why did i put the image in like this?*/}
         <img alt="" className="background-img" src="../mod-5-logo.png" />
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import "./index.css";
 /*todo what's the difference between index.js & app.js? what's the higherarchy?
  *  I'm not totally confident, but I think index.js is the root file, because it is importing app.js */
 import App from "./App";
+import Debugger from "./debugger";
 
 /*todo how can I make my app look best? should I use semantic or not? what matters most for my portfolio?*/
 import "semantic-ui-css/semantic.min.css";
@@ -23,6 +24,7 @@ import { ActionCableProvider } from "react-actioncable-provider";
 import { createStore, applyMiddleware, compose } from "redux"; // lines 17-18; allows for redux devtools
 import reducer from "./redux/reducer";
 import thunk from "redux-thunk";
+import rootReducer from "./redux/reducers";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -33,7 +35,11 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 //   composeEnhancers(applyMiddleware(thunk))
 // );
 
-const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(
+  rootReducer,
+  {},
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 /*todo this is how i get an environment variable. look more into this when I am trying to deploy on heroku*/
 
